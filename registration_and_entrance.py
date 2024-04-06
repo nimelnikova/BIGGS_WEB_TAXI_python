e943b5d008b8b5af486cf4a663a443fb5744e679
Nina Melnikova <ninamelnikova55555@mail.ru>
2024-04-06 21:53:27 +0300

    Свяала бекенд и фронтенд, исправила все косяки с неймингами (в сваггере тоже). И переделала логику формы немного

--- registration_and_entrance.py
+++ registration_and_entrance.py
@@ -2,75 +2,106 @@ from models import User
 from hashlib import sha256
 import json
 import pandas as pd
-from flask import request, Response
+from flask import jsonify, request, Response
 from http import HTTPStatus
+import pathlib
 
+PATH = pathlib.Path(__name__).parent.resolve() / "dataUsers.csv"
 
 
 def registration():
+    DataBase = pd.read_csv(PATH)
 
-    DataBase = pd.read_csv("/home/admin/Second-Semester/ProjectTaxi/BIGGS_WEB_TAXI_python/dataUsers.csv") # считываем имеющуюся базу данных
+    data = request.get_json()  # получаем данные формы пост запросом
 
-    data = request.get_json() # получаем данные формы пост запросом
-
-    hashed_login = sha256() # хэширование данных, используемых для аутентификации
-    hashed_login.update(data["login"].encode()) 
+    hashed_login = sha256()  # хэширование данных, используемых для аутентификации
+    hashed_login.update(data["username"].encode())
     hashed_logemail = sha256()
-    hashed_logemail.update(data["logemail"].encode())
+    hashed_logemail.update(data["email"].encode())
     hashed_logpass = sha256()
-    hashed_logpass.update(data["logpass"].encode())
+    hashed_logpass.update(data["password"].encode())
 
-    user = User(len(DataBase), data["logname"], hashed_login.hexdigest(), hashed_logemail.hexdigest(), hashed_logpass.hexdigest())
+    user = User(
+        len(DataBase),
+        data["fullname"],
+        hashed_login.hexdigest(),
+        hashed_logemail.hexdigest(),
+        hashed_logpass.hexdigest(),
+    )
 
-    if user.login in DataBase["login"].to_list(): # обработка ошибок, связанных с данными
-        return Response("Этот логин уже занят.", HTTPStatus.BAD_REQUEST)
-    elif user.logemail in DataBase["logemail"].to_list():
-        return Response("Аккаунт с такой электронной почтой уже существует.", HTTPStatus.BAD_REQUEST)
+    if user.username in DataBase["username"].to_list():
+        return jsonify({"message": "Этот логин уже занят."}), HTTPStatus.BAD_REQUEST
+    elif user.email in DataBase["email"].to_list():
+        return (
+            jsonify({"message": "Аккаунт с такой электронной почтой уже существует."}),
+            HTTPStatus.BAD_REQUEST,
+        )
 
-    body = { # формирование тела для response
+    body = {  # формирование тела для response
         "user_id": len(DataBase),
-        "logname": user.logname,
-        "login": user.login,
-        "logemail": user.logemail,
-        "logpass": user.get_logpass()
+        "fullname": user.fullname,
+        "username": user.username,
+        "email": user.email,
+        "password": user.get_password(),
     }
 
-    DataBase.loc[len(DataBase.index)] = [user.user_id, user.logname, user.login, user.logemail, user._User__logpass]
-    DataBase.to_csv("/home/admin/Second-Semester/ProjectTaxi/BIGGS_WEB_TAXI_python/dataUsers.csv", index=False)
+    DataBase.loc[len(DataBase.index)] = [
+        user.user_id,
+        user.fullname,
+        user.username,
+        user.email,
+        user._User__password,
+    ]
+    DataBase.to_csv(
+        PATH,
+        index=False,
+    )
 
     return Response(json.dumps(body), HTTPStatus.OK, mimetype="application/json")
 
+
 def entrance():
 
-    DataBase = pd.read_csv("/home/admin/Second-Semester/ProjectTaxi/BIGGS_WEB_TAXI_python/dataUsers.csv")
+    DataBase = pd.read_csv(PATH)
     data = request.get_json()
 
-    hashed_cur_log = sha256() # хэширование полученных данных для сравнения
-    hashed_cur_log.update(data["logemail"].encode())
+    hashed_cur_log = sha256()  # хэширование полученных данных для сравнения
+    hashed_cur_log.update(data["login_or_email"].encode())
     hashed_cur_log = hashed_cur_log.hexdigest()
 
     hashed_cur_logpass = sha256()
-    hashed_cur_logpass.update(data["logpass"].encode())
+    hashed_cur_logpass.update(data["password"].encode())
     hashed_cur_logpass = hashed_cur_logpass.hexdigest()
-
     # проверка полученных данных и формирование запроса
-    if hashed_cur_log in DataBase["login"].to_list() or hashed_cur_log in DataBase["logemail"].to_list():
-        if not DataBase.loc[DataBase["logemail"] == hashed_cur_log]["logpass"].to_list():
-            body = {
-                "user_id": DataBase.loc[DataBase["login"] == hashed_cur_log]["user_id"].to_list()[0],
-                "logname": DataBase.loc[DataBase["login"] == hashed_cur_log]["logname"].to_list()[0],
-                "login": DataBase.loc[DataBase["login"] == hashed_cur_log]["login"].to_list()[0],
-                "logemail": DataBase.loc[DataBase["login"] == hashed_cur_log]["logemail"].to_list()[0],
-                "logpass": DataBase.loc[DataBase["login"] == hashed_cur_log]["logpass"].to_list()[0]
-            }
-            return Response(json.dumps(body), HTTPStatus.OK, mimetype="application/json")
+    user_row = None
+
+    # Попытка найти пользователя по username или email
+    if (
+        hashed_cur_log in DataBase["username"].values
+        or hashed_cur_log in DataBase["email"].values
+    ):
+        user_row = DataBase.loc[
+            (DataBase["username"] == hashed_cur_log)
+            | (DataBase["email"] == hashed_cur_log)
+        ]
+
+    if user_row is not None and not user_row.empty:
+        # Получаем первую строку, соответствующую пользователю
+        user_data = user_row.iloc[0]
+
+        # Подготавливаем данные пользователя для ответа
         body = {
-                "user_id": DataBase.loc[DataBase["logemail"] == hashed_cur_log]["user_id"].to_list()[0],
-                "logname": DataBase.loc[DataBase["logemail"] == hashed_cur_log]["logname"].to_list()[0],
-                "login": DataBase.loc[DataBase["logemail"] == hashed_cur_log]["login"].to_list()[0],
-                "logemail": DataBase.loc[DataBase["logemail"] == hashed_cur_log]["logemail"].to_list()[0],
-                "logpass": DataBase.loc[DataBase["logemail"] == hashed_cur_log]["logpass"].to_list()[0]
-            }
+            "user_id": str(user_data["user_id"]),
+            "fullname": user_data["fullname"],
+            "username": user_data["username"],
+            "email": user_data["email"],
+            "password": user_data["password"],
+        }
+        print(body)
+        # Отправляем ответ
         return Response(json.dumps(body), HTTPStatus.OK, mimetype="application/json")
 
-    return Response("Неверный адрес электронной почты или логин.", HTTPStatus.BAD_REQUEST)
+    return (
+        jsonify({"message": "Неверный адрес электронной почты или логин."}),
+        HTTPStatus.BAD_REQUEST,
+    )
