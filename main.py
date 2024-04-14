9f86f13d4c2c621c16508a2279eaadb5a59c65dc
admin <admin@ubuntu.myguest.virtualbox.org>
2024-04-14 16:06:54 +0300

    БД

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
+import connexion
 from flask import render_template, Response
 from http import HTTPStatus
-import connexion
+import pathlib
+import sqlite3
+import sqlite_query
+
 
 app = connexion.App(__name__, specification_dir="./")
 app.add_api("swagger.yml")
+app.debug = True
 
+PATH = pathlib.Path(__name__).parent.resolve() / "dataUsers.db"
 
 @app.route("/")
 def index():
     return render_template("index.html")  # главное окно с формой
 
 
+conn = sqlite3.connect(PATH) # создаем базу данных, если ее еще нет
+cur = conn.cursor()
+cur.execute(sqlite_query.create_table)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(port=8000)
--- registration_and_entrance.py
+++ registration_and_entrance.py
@@ -5,101 +5,136 @@ import pandas as pd
 from flask import jsonify, request, Response
 from http import HTTPStatus
 import pathlib
+import sqlite_query
+import sqlite3
 
-PATH = pathlib.Path(__name__).parent.resolve() / "dataUsers.csv"
+
+PATH = pathlib.Path(__name__).parent.resolve() / "dataUsers.db"
 
 
 def registration():
-    DataBase = pd.read_csv(PATH)
+    conn = sqlite3.connect(PATH) # создаем базу данных, если ее еще нет
+    cur = conn.cursor()
 
     data = request.get_json()  # получаем данные формы пост запросом
 
-    hashed_login = sha256()  # хэширование данных, используемых для аутентификации
+    hashed_login = sha256()  # Хэширование данных, используемых для аутентификации
     hashed_login.update(data["username"].encode())
+    hashed_login_result = hashed_login.hexdigest()
+
     hashed_logemail = sha256()
     hashed_logemail.update(data["email"].encode())
-    hashed_logpass = sha256()
-    hashed_logpass.update(data["password"].encode())
+    hashed_logemail_result = hashed_logemail.hexdigest()
+
+    hashed_password = sha256()
+    hashed_password.update(data["password"].encode())
+    hashed_password_result = hashed_password.hexdigest()
 
     user = User(
-        len(DataBase),
         data["fullname"],
-        hashed_login.hexdigest(),
-        hashed_logemail.hexdigest(),
-        hashed_logpass.hexdigest(),
+        hashed_login_result,
+        hashed_logemail_result,
+        hashed_password_result,
     )
 
-    if user.username in DataBase["username"].to_list():
+    # проверка на существование пользователя с таким логином
+    cur.execute(sqlite_query.check_user_login, (user.username,))
+    exists = cur.fetchone()[0]
+
+    if exists:
         return jsonify({"message": "Этот логин уже занят."}), HTTPStatus.BAD_REQUEST
-    elif user.email in DataBase["email"].to_list():
+    
+    # проверка на существование пользователя с такой почтой 
+    cur.execute(sqlite_query.check_user_email, (user.email,))
+    exists = cur.fetchone()[0]
+
+    if exists:
         return (
             jsonify({"message": "Аккаунт с такой электронной почтой уже существует."}),
             HTTPStatus.BAD_REQUEST,
         )
 
+    cur.execute(sqlite_query.insert_user,
+        (
+        user.fullname,
+        user.username,
+        user.email,
+        user._User__password,
+        )
+    )
+
+    user_id = cur.execute(sqlite_query.find_id, (user.username,))
+
     body = {  # формирование тела для response
-        "user_id": len(DataBase),
+        "id": user_id.fetchall()[0][0],
         "fullname": user.fullname,
         "username": user.username,
         "email": user.email,
         "password": user.get_password(),
     }
 
-    DataBase.loc[len(DataBase.index)] = [
-        user.user_id,
-        user.fullname,
-        user.username,
-        user.email,
-        user._User__password,
-    ]
-    DataBase.to_csv(
-        PATH,
-        index=False,
-    )
-
+    conn.commit()
+    conn.close()
     return Response(json.dumps(body), HTTPStatus.OK, mimetype="application/json")
 
 
 def entrance():
 
-    DataBase = pd.read_csv(PATH)
+    conn = sqlite3.connect(PATH)
+    cur = conn.cursor()
+
     data = request.get_json()
 
-    hashed_cur_log = sha256()  # хэширование полученных данных для сравнения
-    hashed_cur_log.update(data["login_or_email"].encode())
-    hashed_cur_log = hashed_cur_log.hexdigest()
+    # хэширование полученных данных для сравнения
+    hashed_log = sha256()
+    hashed_log.update(data["login_or_email"].encode())
+    hashed_log_result = hashed_log.hexdigest()
 
-    hashed_cur_logpass = sha256()
-    hashed_cur_logpass.update(data["password"].encode())
-    hashed_cur_logpass = hashed_cur_logpass.hexdigest()
-    # проверка полученных данных и формирование запроса
-    user_row = None
+    hashed_password = sha256()
+    hashed_password.update(data["password"].encode())
+    hashed_password_result = hashed_password.hexdigest()
 
     # Попытка найти пользователя по username или email
+    cur.execute(sqlite_query.check_user_login, (hashed_log_result,))
+    login_exists = cur.fetchone()[0]
+
+    cur.execute(sqlite_query.check_user_email, (hashed_log_result,))
+    email_exists = cur.fetchone()[0]
+
+    # Получаем данные пользователя по логину или почте пользователя
+    cur.execute(sqlite_query.check_user, (hashed_log_result, hashed_log_result,))
+    rows = cur.fetchone()
+    data = [0] * 5
+    for i, row in enumerate(rows):
+        data[i] = row
+
+    cur_id = data[0]
+    cur_name = data[1]
+    cur_login = data[2]
+    cur_email = data[3]
+    cur_password = data[4]
+
+
     if (
-        hashed_cur_log in DataBase["username"].values
-        or hashed_cur_log in DataBase["email"].values
+        login_exists or email_exists
     ):
-        user_row = DataBase.loc[
-            (DataBase["username"] == hashed_cur_log)
-            | (DataBase["email"] == hashed_cur_log)
-        ]
-
-    if user_row is not None and not user_row.empty:
-        # Получаем первую строку, соответствующую пользователю
-        user_data = user_row.iloc[0]
-
-        # Подготавливаем данные пользователя для ответа
-        body = {
-            "user_id": str(user_data["user_id"]),
-            "fullname": user_data["fullname"],
-            "username": user_data["username"],
-            "email": user_data["email"],
-            "password": user_data["password"],
-        }
-        print(body)
-        # Отправляем ответ
-        return Response(json.dumps(body), HTTPStatus.OK, mimetype="application/json")
+        if ( # Сравниваем пароль пользователя с переданным
+            hashed_password_result == cur_password
+        ):
+            body = { # Формируем тело пользователя для ответа
+                "user_id": cur_id,
+                "fullname": cur_name,
+                "username": cur_login,
+                "email": cur_email,
+                "password": cur_password,
+            }
+            # Отправляем ответ
+            return Response(json.dumps(body), HTTPStatus.OK, mimetype="application/json")
+        
+        return (
+            jsonify({"message": "Неверный пароль."}),
+            HTTPStatus.BAD_REQUEST,
+            )
 
     return (
         jsonify({"message": "Неверный адрес электронной почты или логин."}),
