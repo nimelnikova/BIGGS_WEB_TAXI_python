413e78de4b0779eb6560d2a7df84aa4f1f3835f3
SurovIvan <iesurov@mail.education>
2024-04-22 16:42:40 +0300

    добавил бд для заказов

--- sqlite_query.py
+++ sqlite_query.py
@@ -19,3 +19,20 @@ check_user_email = """SELECT EXISTS(SELECT 1 FROM users WHERE email = ?);"""
 find_id = """SELECT id FROM users WHERE username = ?;"""
 
 check_user = """SELECT * FROM users WHERE username = ? OR email = ?;"""
+
+
+create_table_orders = """CREATE TABLE IF NOT EXISTS orders(
+                order_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                customer_id INTEGER,
+                pickup_location TEXT,
+                destination TEXT,
+                distance REAL,
+                car_category TEXT,
+                start_time TEXT,
+                end_time TEXT,
+                total_ride_time INTEGER,
+                order_amount INTEGER
+
+    )"""
+
+insert_orders = """INSERT INTO orders (customer_id, pickup_location, destination, distance, car_category, start_time, end_time, total_ride_time, order_amount) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);"""
